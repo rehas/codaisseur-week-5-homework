594f28bb0204ad91426fafae225eb7d547e4017b
RehaS <beratson@gmail.com>
2018-08-04 00:16:26 +0200

    base selector with actions and store checks implemented

--- src/containers/PizzaBaseContainer.js
+++ src/containers/PizzaBaseContainer.js
@@ -1,13 +1,28 @@
 import React, {PureComponent} from 'react'
+import {chooseBase} from '../actions/pizzabase'
+import {connect} from 'react-redux'
+import PizzaBaseForm from './PizzaBaseForm'
+
 
 class PizzaBaseContainer extends PureComponent{
+  handleBaseFormChange = (selection)=>{
+    this.props.chooseBase(selection)
+  }
+
   render(){
     return (
       <div>
-        PizzaBaseContainer
+        <PizzaBaseForm handleChange = {this.handleBaseFormChange}/>
       </div>
     )
   }
 }
 
-export default PizzaBaseContainer
+
+const mapStateToProps = (state) =>{
+  return {
+    sauce: state.pizzasauce
+  }
+}
+
+export default connect(mapStateToProps, {chooseBase})(PizzaBaseContainer)
---
+++ src/containers/PizzaBaseForm.js
@@ -0,0 +1,80 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
+
+const styles = theme => ({
+  button: {
+    display: "block",
+    marginTop: theme.spacing.unit * 2
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120
+  }
+});
+
+class ControlledOpenSelect extends React.Component {
+  state = {
+    age: "",
+    open: false
+  };
+
+  handleChange = event => {
+    this.props.handleChange(event.target.value) 
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <form autoComplete="off">
+        <Button className={classes.button} onClick={this.handleOpen}>
+          Open the select
+        </Button>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="demo-controlled-open-select">Size</InputLabel>
+          <Select
+            open={this.state.open}
+            onClose={this.handleClose}
+            onOpen={this.handleOpen}
+            value={this.state.age}
+            onChange={this.handleChange}
+            inputProps={{
+              name: "age",
+              id: "demo-controlled-open-select"
+            }}
+          >
+            <MenuItem value={null}>
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={25}>25cm NY Style € 8,99</MenuItem>
+            <MenuItem value={30}>30cm NY Style € 11,49</MenuItem>
+            <MenuItem value={35}>35cm NY Style € 13,49</MenuItem>
+          </Select>
+        </FormControl>
+      </form>
+    );
+  }
+}
+
+ControlledOpenSelect.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+
+
+export default withStyles(styles)(ControlledOpenSelect);
